3b340af864dca586ad80eadf2a786bd529d2e9a1
Ondřej Karmazín <ondrej.karmazin@outlook.com>
2025-10-13 14:57:25 +0200

    Set JVM target 17

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -14,7 +14,11 @@ version = "0.0.1"
 
 kotlin {
     explicitApi()
-    jvm()
+    jvm {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
     js {
         browser()
         nodejs()
@@ -26,7 +30,7 @@ kotlin {
         publishLibraryVariants("release")
         @OptIn(ExperimentalKotlinGradlePluginApi::class)
         compilerOptions {
-            jvmTarget.set(JvmTarget.JVM_11)
+            jvmTarget.set(JvmTarget.JVM_17)
         }
     }
     iosX64()
@@ -58,8 +62,8 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 }
 
